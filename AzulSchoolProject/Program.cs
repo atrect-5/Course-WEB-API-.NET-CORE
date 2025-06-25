7339df1bef319475d80fd27e05ff9a726971fc50
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-06-25 17:31:35 -0600

    Adding Dependencies and Conection to progresSql

--- AzulSchoolProject/Program.cs
+++ AzulSchoolProject/Program.cs
@@ -1,15 +1,21 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ProjectDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ProjectServer")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
