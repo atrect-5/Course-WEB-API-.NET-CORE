5e1b8e64d436ff337055865b2882e2459831f7a8
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-07-03 13:58:53 -0600

    Add GetAllUsers, UpdateUser and deleteUser

--- AzulSchoolProject/Controllers/UserController.cs
+++ AzulSchoolProject/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Dtos;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 using Services;
 
 namespace AzulSchoolProject.Controllers
@@ -16,10 +17,16 @@ namespace AzulSchoolProject.Controllers
         {
             var user = _userService.GetUserById(id);
             if (user is null)
-            {
                 return NotFound(); // Return 404 Not Found if the user does not exist
-            }
+            
             return Ok(user); // Return 200 OK with the user data
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = _userService.GetAllUsers();
+            return Ok(users); // Return 200 OK with the list of users
         }
 
         [HttpPost]
@@ -31,6 +38,25 @@ namespace AzulSchoolProject.Controllers
             // This includes the URI of the newly created resource in the Location header
             // Also includes the created user in the response body
             return CreatedAtAction(nameof(GetUserById), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] CreateUserDto createUserDto)
+        {
+            var updatedUser = _userService.Update(id, createUserDto);
+            if (updatedUser is null)
+                return NotFound();
+
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id) 
+        {
+            bool isDeleted = _userService.Delete(id);
+            if (!isDeleted)
+                return NotFound();
+            return NoContent(); // 204 No Content
         }
     }
 }
