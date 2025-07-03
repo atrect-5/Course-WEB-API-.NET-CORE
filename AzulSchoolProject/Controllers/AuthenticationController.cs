7e13d1841bfeebe51997dfaa3f793ef14e78963b
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-07-02 22:38:48 -0600

    Add GetUserById and Add user Controllers

---
+++ AzulSchoolProject/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace AzulSchoolProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController(IAuthenticationService authenticationService) : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService = authenticationService;
+    }
+}
--- AzulSchoolProject/Controllers/UserController.cs
+++ AzulSchoolProject/Controllers/UserController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dtos;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Services;
 
 namespace AzulSchoolProject.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserController(IUserService userService) : ControllerBase
     {
+        private readonly IUserService _userService = userService;
+        
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user is null)
+            {
+                return NotFound(); // Return 404 Not Found if the user does not exist
+            }
+            return Ok(user); // Return 200 OK with the user data
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] CreateUserDto createUserDto)
+        {
+            var newUser = _userService.Add(createUserDto);
+
+            // Return status 201 Created.
+            // This includes the URI of the newly created resource in the Location header
+            // Also includes the created user in the response body
+            return CreatedAtAction(nameof(GetUserById), new { id = newUser.Id }, newUser);
+        }
     }
 }
