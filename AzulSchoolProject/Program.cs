db2e07d1c73b520943b43f2146255d1c20ec7479
Alejandro Gonzalez <alex1123.nel@gmail.com>
2025-07-17 21:45:34 -0600

    Add swagger to deploy

--- AzulSchoolProject/Program.cs
+++ AzulSchoolProject/Program.cs
@@ -114,12 +114,9 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.  
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 
